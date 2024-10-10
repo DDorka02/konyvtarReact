901d201e49f843175dcf198c616be4afbe0ae272
DDorka02 <dobszaydorka@gmail.com>
2024-10-10 23:52:28 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>Könyváruház</h1>
+        </header>
+        <main className="App-main">
+          <div className="fokonyv">
+            <h3>Könyvek</h3>
+            <div className="kiemelt">
+              valami
+            </div>
+            <p>Leírás leírás leírás</p>
+        </div>
+        <div className="konyvek">
+
+          </div>
+        </main>
+        <footer>
+          <p>Dobszay Dorka</p>
+        </footer>
+    </div>
+  );
+}
+
+export default App;
